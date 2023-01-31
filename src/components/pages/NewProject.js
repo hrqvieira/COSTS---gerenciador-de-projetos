94ed1ae0ab25deb4638484bccfc0bf845a3398fe
Henrique Vieira <vieira.henrique02@outlook.com>
2023-01-31 20:42:34 -0300

    Inserindo projeto no banco

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,39 @@
-import styles from './NewProject.module.css'
+import styles from "./NewProject.module.css";
+import { useNavigate } from "react-router-dom";
 
-import ProjectForm from '../project/ProjectForm'
+import ProjectForm from "../project/ProjectForm";
 
 function NewProject() {
-    return(
-        <div className={styles.newproject_container}>
-            <h1>Criar projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar projeto" />
-        </div>
-    )
+  const history = useNavigate();
+
+  function createPost(project) {
+    // initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        //redirect
+        history('/projects', {message: 'Projeto criado com sucesso'})
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
