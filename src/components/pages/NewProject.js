a22c1e105a3cc9fb7ae6eea29ed66c5eb97f2d9b
Henrique Vieira <vieira.henrique02@outlook.com>
2023-01-31 23:33:32 -0300

    criacao da message e inicio da pagina projects

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import ProjectForm from "../project/ProjectForm";
 
 function NewProject() {
-  const history = useNavigate();
+  const navigate = useNavigate();
 
   function createPost(project) {
     // initialize cost and services
@@ -22,7 +22,7 @@ function NewProject() {
       .then((data) => {
         console.log(data);
         //redirect
-        history('/projects', {message: 'Projeto criado com sucesso'})
+        navigate('/projects', { state: {message: 'Projeto criado com sucesso!'} })
       })
       .catch((err) => console.log(err));
   }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,30 @@
+import Message from "../layout/Message";
+import { useLocation } from "react-router";
+import Container from "../layout/Container";
+import styles from "./Projects.module.css";
+import LinkButton from "../layout/LinkButton";
+
 function Projects() {
-    return <h1>Projetos</h1>
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+      </div>    
+
+      {message && <Message msg={message} type="sucess" />}
+
+      <Container customClass="start">
+        <p>Projetos</p>
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
